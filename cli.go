ca72e145d8fc6d11046142b97213324dd85543d7
Ibrahim Khan <ibrahim.khan@cefalo.com>
2022-07-15 11:54:11 +0600

    Finish deployment settings ui

--- cli.go
+++ cli.go
@@ -9,19 +9,24 @@ import (
 
 const (
 	__pageNameSetSelection string = "Select Deployment Set"
-	__pageNameDeployment          = "Deployment"
+	__pageNameDeployment   string = "Deployment"
 )
 
 func main() {
 	config := config.ReadConfigOrPanic()
 	app := tview.NewApplication()
-	setSelection := ui.NewSetSelectionLayout(config)
 	pages := tview.NewPages()
+	pageSelection := ui.NewSetSelectionLayout(config, func() {
+		pages.SwitchToPage(__pageNameDeployment)
+		pages.SetTitle(__pageNameDeployment)
+	})
+	pageDeployment := tview.NewBox().SetTitle("Deployment")
 
 	pages.SetBorder(true).
 		SetTitle(__pageNameSetSelection)
 
-	pages.AddPage(__pageNameSetSelection, setSelection, true, true)
+	pages.AddPage(__pageNameSetSelection, pageSelection, true, true)
+	pages.AddPage(__pageNameDeployment, pageDeployment, true, false)
 
 	if err := app.SetRoot(pages, true).Run(); err != nil {
 		panic(err)
--- ui/setSelection.go
+++ ui/setSelection.go
@@ -3,17 +3,57 @@ package ui
 import (
 	"az-func-deploy/config"
 
+	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 )
 
-func NewSetSelectionLayout(dcon *config.DeployConfig) *tview.Form {
+func removeFuncs(form *tview.Form, dcon *config.DeployConfig) {
+	currentSet := dcon.Sets[dcon.CurrentSet]
+	for _, funcInfo := range currentSet.FuncInfos {
+		form.RemoveFormItem(form.GetFormItemIndex(funcInfo.FuncName))
+	}
+}
+
+func addFuncs(form *tview.Form, dcon *config.DeployConfig) {
+	currentSet := dcon.Sets[dcon.CurrentSet]
+	for i, funcInfo := range currentSet.FuncInfos {
+		form.AddCheckbox(funcInfo.FuncName, funcInfo.ShouldRun, func(checked bool) {
+			dcon.Sets[dcon.CurrentSet].FuncInfos[i].ShouldRun = checked
+			dcon.WriteConfig()
+		})
+	}
+}
+
+func NewSetSelectionLayout(dcon *config.DeployConfig, onDeploy func()) *tview.Form {
 	form := tview.NewForm()
 	optionsSet := []string{}
 	for _, set := range dcon.Sets {
 		optionsSet = append(optionsSet, set.Name)
 	}
-	form.AddDropDown("Deployment Set", optionsSet, dcon.CurrentSet, func(option string, optionIndex int) {})
+	form.AddDropDown("Deployment Set", optionsSet, dcon.CurrentSet, func(_ string, optionIndex int) {
+		if optionIndex != dcon.CurrentSet {
+			removeFuncs(form, dcon)
+			dcon.CurrentSet = optionIndex
+			addFuncs(form, dcon)
+			dcon.WriteConfig()
+		}
+	})
 	optionsMethods := config.GetDeploymentMethods()
-	form.AddDropDown("Deployment Set", optionsMethods, config.GetDeploymentMethodIndex(dcon.Method), func(option string, optionIndex int) {})
+	form.AddDropDown("Deployment Method", optionsMethods, config.GetDeploymentMethodIndex(dcon.Method), func(option string, _ int) {
+		if option != dcon.Method {
+			dcon.Method = option
+			dcon.WriteConfig()
+		}
+	})
+	addFuncs(form, dcon)
+	form.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		if event.Rune() == 'd' {
+			onDeploy()
+			return nil
+		}
+		return event
+	})
+	form.AddButton("(d) Deploy", onDeploy)
+
 	return form
 }
