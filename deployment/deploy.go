f1ec0baede1225b5215317048cf0466ec929bb86
Ibrahim Khan <ibrahim.khan@cefalo.com>
2022-07-20 15:22:19 +0600

    Show list of deployed functions at the end

--- deployment/deploy.go
+++ deployment/deploy.go
@@ -20,11 +20,11 @@ func DeployFunctions(conf *config.DeployConfig, writer io.Writer, disableColor b
 	time.Sleep(5 * time.Second)
 	logger.Highlightln("Starting Deployment")
 	totalFuncs := len(currentSet.FuncInfos)
-	deployCount := 0
+	deployedFuncs := []string{}
 	for i, funcInfo := range currentSet.FuncInfos {
 		logger.SetScope(fmt.Sprintf("%d/%d | %s", i+1, totalFuncs, funcInfo.FuncName))
 		logger.ScopedBlackYellowln("Deploying Function")
-		logger.ScopedBlackYellowln(funcInfo.ProjectDir)
+		logger.BlackYellowln(funcInfo.ProjectDir)
 		if !funcInfo.ShouldRun {
 			logger.ScopedBlackRedln("Skipped")
 			continue
@@ -44,9 +44,12 @@ func DeployFunctions(conf *config.DeployConfig, writer io.Writer, disableColor b
 			}
 			os.Remove(outputFile)
 		}
-		deployCount += 1
+		deployedFuncs = append(deployedFuncs, funcInfo.FuncName)
 		logger.ScopedBlackYellowln("End")
 	}
-	logger.Highlightln(fmt.Sprintf("Deployed %d of %d functions", deployCount, totalFuncs))
+	logger.Highlightln(fmt.Sprintf("Deployed %d of %d functions", len(deployedFuncs), totalFuncs))
+	for _, name := range deployedFuncs {
+		logger.Greenln(name)
+	}
 	logger.Highlightln("Finised Deployment")
 }
